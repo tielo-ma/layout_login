7d12558de5e59a91448d00d6605b5ad745ff6205
Marcelo <90ma.celo@gmail.com>
2025-01-20 17:44:13 +0000

    configurando minha classe juntamente com os atributos dos produtos

---
+++ layout.py
@@ -0,0 +1,7 @@
+import flet as ft
+#criando uma classe para meus produtos
+#irei criar uns atributos dentro da classe
+class Produto(self, nome, preco, descricao):
+    self.nome = nome
+    self.preco = preco
+    self.descricao = descricao
